085802b87d03b3992e74ee6f541905bbde7c1607
emanuel71jo <emanuel71jo@gmail.com>
2021-06-19 18:48:35 -0300

    Terceiro dia do nlw 5, trilha react native

---
+++ src/components/EnvironmentButton.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { StyleSheet, Text } from "react-native";
+import { RectButton, RectButtonProps } from "react-native-gesture-handler";
+import colors from "../styles/colors";
+import fonts from "../styles/fonts";
+
+interface EnvironmentButtonProps extends RectButtonProps {
+  title: string;
+  active?: boolean;
+}
+
+export function EnvironmentButton({
+  title,
+  active,
+  ...rest
+}: EnvironmentButtonProps) {
+  return (
+    <RectButton
+      style={[styles.button, active && styles.buttonActive]}
+      {...rest}
+    >
+      <Text style={[styles.text, active && styles.textActive]}>{title}</Text>
+    </RectButton>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: colors.shape,
+    height: 40,
+    width: 76,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 12,
+    marginHorizontal: 5,
+  },
+  buttonActive: {
+    backgroundColor: colors.green_light,
+  },
+  text: {
+    color: colors.heading,
+    fontFamily: fonts.text,
+  },
+  textActive: {
+    color: colors.green_dark,
+    fontFamily: fonts.heading,
+  },
+});
